5b66c420ced1328c2c9696891e0e2e9278afd56f
badoutech2017@gmail.com <badoutech2017@gmail.com>
2016-07-27 19:43:28 +0800

    update service js

--- js/smoothscroll.js
+++ js/smoothscroll.js
@@ -1,303 +1,33 @@
-// SmoothScroll v0.9.9
-// Licensed under the terms of the MIT license.
-// People involved
-// - Balazs Galambosi: maintainer (CHANGELOG.txt)
-// - Patrick Brunner (patrickb1991@gmail.com)
-// - Michael Herf: ssc_pulse Algorithm
+/**
+ * SmoothScroll
+ * This helper script created by DWUser.com.  Copyright 2013 DWUser.com.  
+ * Dual-licensed under the GPL and MIT licenses.  
+ * All individual scripts remain property of their copyrighters.
+ * Date: 10-Sep-2013
+ * Version: 1.0.1
+ */
+if (!window['jQuery']) alert('The jQuery library must be included before the smoothscroll.js file.  The plugin will not work propery.');
 
-function ssc_init() {
-  if (!document.body) return;
-  var e = document.body;
-  var t = document.documentElement;
-  var n = window.innerHeight;
-  var r = e.scrollHeight;
-  ssc_root = document.compatMode.indexOf("CSS") >= 0 ? t : e;
-  ssc_activeElement = e;
-  ssc_initdone = true;
-  if (top != self) {
-    ssc_frame = true
-  } else if (r > n && (e.offsetHeight <= n || t.offsetHeight <= n)) {
-    ssc_root.style.height = "auto";
-    if (ssc_root.offsetHeight <= n) {
-      var i = document.createElement("div");
-      i.style.clear = "both";
-      e.appendChild(i)
-    }
-  }
-  if (!ssc_fixedback) {
-    e.style.backgroundAttachment = "scroll";
-    t.style.backgroundAttachment = "scroll"
-  }
-  if (ssc_keyboardsupport) {
-    ssc_addEvent("keydown", ssc_keydown)
-  }
-}
+/**
+ * jQuery.ScrollTo - Easy element scrolling using jQuery.
+ * Copyright (c) 2007-2013 Ariel Flesler - aflesler(at)gmail(dot)com | http://flesler.blogspot.com
+ * Dual licensed under MIT and GPL.
+ * @author Ariel Flesler
+ * @version 1.4.3.1
+ */
+;(function($){var h=$.scrollTo=function(a,b,c){$(window).scrollTo(a,b,c)};h.defaults={axis:'xy',duration:parseFloat($.fn.jquery)>=1.3?0:1,limit:true};h.window=function(a){return $(window)._scrollable()};$.fn._scrollable=function(){return this.map(function(){var a=this,isWin=!a.nodeName||$.inArray(a.nodeName.toLowerCase(),['iframe','#document','html','body'])!=-1;if(!isWin)return a;var b=(a.contentWindow||a).document||a.ownerDocument||a;return/webkit/i.test(navigator.userAgent)||b.compatMode=='BackCompat'?b.body:b.documentElement})};$.fn.scrollTo=function(e,f,g){if(typeof f=='object'){g=f;f=0}if(typeof g=='function')g={onAfter:g};if(e=='max')e=9e9;g=$.extend({},h.defaults,g);f=f||g.duration;g.queue=g.queue&&g.axis.length>1;if(g.queue)f/=2;g.offset=both(g.offset);g.over=both(g.over);return this._scrollable().each(function(){if(e==null)return;var d=this,$elem=$(d),targ=e,toff,attr={},win=$elem.is('html,body');switch(typeof targ){case'number':case'string':if(/^([+-]=)?\d+(\.\d+)?(px|%)?$/.test(targ)){targ=both(targ);break}targ=$(targ,this);if(!targ.length)return;case'object':if(targ.is||targ.style)toff=(targ=$(targ)).offset()}$.each(g.axis.split(''),function(i,a){var b=a=='x'?'Left':'Top',pos=b.toLowerCase(),key='scroll'+b,old=d[key],max=h.max(d,a);if(toff){attr[key]=toff[pos]+(win?0:old-$elem.offset()[pos]);if(g.margin){attr[key]-=parseInt(targ.css('margin'+b))||0;attr[key]-=parseInt(targ.css('border'+b+'Width'))||0}attr[key]+=g.offset[pos]||0;if(g.over[pos])attr[key]+=targ[a=='x'?'width':'height']()*g.over[pos]}else{var c=targ[pos];attr[key]=c.slice&&c.slice(-1)=='%'?parseFloat(c)/100*max:c}if(g.limit&&/^\d+$/.test(attr[key]))attr[key]=attr[key]<=0?0:Math.min(attr[key],max);if(!i&&g.queue){if(old!=attr[key])animate(g.onAfterFirst);delete attr[key]}});animate(g.onAfter);function animate(a){$elem.animate(attr,f,g.easing,a&&function(){a.call(this,e,g)})}}).end()};h.max=function(a,b){var c=b=='x'?'Width':'Height',scroll='scroll'+c;if(!$(a).is('html,body'))return a[scroll]-$(a)[c.toLowerCase()]();var d='client'+c,html=a.ownerDocument.documentElement,body=a.ownerDocument.body;return Math.max(html[scroll],body[scroll])-Math.min(html[d],body[d])};function both(a){return typeof a=='object'?a:{top:a,left:a}}})(jQuery);
 
-function ssc_scrollArray(e, t, n, r) {
-  r || (r = 1e3);
-  ssc_directionCheck(t, n);
-  ssc_que.push({
-    x: t,
-    y: n,
-    lastX: t < 0 ? .99 : -.99,
-    lastY: n < 0 ? .99 : -.99,
-    start: +(new Date)
-  });
-  if (ssc_pending) {
-    return
-  }
-  var i = function() {
-    var s = +(new Date);
-    var o = 0;
-    var u = 0;
-    for (var a = 0; a < ssc_que.length; a++) {
-      var f = ssc_que[a];
-      var l = s - f.start;
-      var c = l >= ssc_animtime;
-      var h = c ? 1 : l / ssc_animtime;
-      if (ssc_pulseAlgorithm) {
-        h = ssc_pulse(h)
-      }
-      var p = f.x * h - f.lastX >> 0;
-      var d = f.y * h - f.lastY >> 0;
-      o += p;
-      u += d;
-      f.lastX += p;
-      f.lastY += d;
-      if (c) {
-        ssc_que.splice(a, 1);
-        a--
-      }
-    }
-    if (t) {
-      var v = e.scrollLeft;
-      e.scrollLeft += o;
-      if (o && e.scrollLeft === v) {
-        t = 0
-      }
-    }
-    if (n) {
-      var m = e.scrollTop;
-      e.scrollTop += u;
-      if (u && e.scrollTop === m) {
-        n = 0
-      }
-    }
-    if (!t && !n) {
-      ssc_que = []
-    }
-    if (ssc_que.length) {
-      setTimeout(i, r / ssc_framerate + 1)
-    } else {
-      ssc_pending = false
-    }
-  };
-  setTimeout(i, 0);
-  ssc_pending = true
-}
+/**
+ * jQuery.LocalScroll
+ * Copyright (c) 2007-2010 Ariel Flesler - aflesler(at)gmail(dot)com | http://flesler.blogspot.com
+ * Dual licensed under MIT and GPL.
+ * Date: 05/31/2010
+ * @author Ariel Flesler
+ * @version 1.2.8b
+ **/
+;(function(b){function g(a,e,d){var h=e.hash.slice(1),f=document.getElementById(h)||document.getElementsByName(h)[0];if(f){a&&a.preventDefault();var c=b(d.target);if(!(d.lock&&c.is(":animated")||d.onBefore&&!1===d.onBefore(a,f,c))){d.stop&&c._scrollable().stop(!0);if(d.hash){var a=f.id==h?"id":"name",g=b("<a> </a>").attr(a,h).css({position:"absolute",top:b(window).scrollTop(),left:b(window).scrollLeft()});f[a]="";b("body").prepend(g);location=e.hash;g.remove();f[a]=h}c.scrollTo(f,d).trigger("notify.serialScroll",
+[f])}}}var i=location.href.replace(/#.*/,""),c=b.localScroll=function(a){b("body").localScroll(a)};c.defaults={duration:1E3,axis:"y",event:"click",stop:!0,target:window,reset:!0};c.hash=function(a){if(location.hash){a=b.extend({},c.defaults,a);a.hash=!1;if(a.reset){var e=a.duration;delete a.duration;b(a.target).scrollTo(0,a);a.duration=e}g(0,location,a)}};b.fn.localScroll=function(a){function e(){return!!this.href&&!!this.hash&&this.href.replace(this.hash,"")==i&&(!a.filter||b(this).is(a.filter))}
+a=b.extend({},c.defaults,a);return a.lazy?this.bind(a.event,function(d){var c=b([d.target,d.target.parentNode]).filter(e)[0];c&&g(d,c,a)}):this.find("a,area").filter(e).bind(a.event,function(b){g(b,this,a)}).end().end()}})(jQuery);
 
-function ssc_wheel(e) {
-  if (!ssc_initdone) {
-    ssc_init()
-  }
-  var t = e.target;
-  var n = ssc_overflowingAncestor(t);
-  if (!n || e.defaultPrevented || ssc_isNodeName(ssc_activeElement, "embed") || ssc_isNodeName(t, "embed") && /\.pdf/i.test(t.src)) {
-    return true
-  }
-  var r = e.wheelDeltaX || 0;
-  var i = e.wheelDeltaY || 0;
-  if (!r && !i) {
-    i = e.wheelDelta || 0
-  }
-  if (Math.abs(r) > 1.2) {
-    r *= ssc_stepsize / 120
-  }
-  if (Math.abs(i) > 1.2) {
-    i *= ssc_stepsize / 120
-  }
-  ssc_scrollArray(n, -r, -i);
-  e.preventDefault()
-}
-
-function ssc_keydown(e) {
-  var t = e.target;
-  var n = e.ctrlKey || e.altKey || e.metaKey;
-  if (/input|textarea|embed/i.test(t.nodeName) || t.isContentEditable || e.defaultPrevented || n) {
-    return true
-  }
-  if (ssc_isNodeName(t, "button") && e.keyCode === ssc_key.spacebar) {
-    return true
-  }
-  var r, i = 0,
-      s = 0;
-  var o = ssc_overflowingAncestor(ssc_activeElement);
-  var u = o.clientHeight;
-  if (o == document.body) {
-    u = window.innerHeight
-  }
-  switch (e.keyCode) {
-  case ssc_key.up:
-    s = -ssc_arrowscroll;
-    break;
-  case ssc_key.down:
-    s = ssc_arrowscroll;
-    break;
-  case ssc_key.spacebar:
-    r = e.shiftKey ? 1 : -1;
-    s = -r * u * .9;
-    break;
-  case ssc_key.pageup:
-    s = -u * .9;
-    break;
-  case ssc_key.pagedown:
-    s = u * .9;
-    break;
-  case ssc_key.home:
-    s = -o.scrollTop;
-    break;
-  case ssc_key.end:
-    var a = o.scrollHeight - o.scrollTop - u;
-    s = a > 0 ? a + 10 : 0;
-    break;
-  case ssc_key.left:
-    i = -ssc_arrowscroll;
-    break;
-  case ssc_key.right:
-    i = ssc_arrowscroll;
-    break;
-  default:
-    return true
-  }
-  ssc_scrollArray(o, i, s);
-  e.preventDefault()
-}
-
-function ssc_mousedown(e) {
-  ssc_activeElement = e.target
-}
-
-function ssc_setCache(e, t) {
-  for (var n = e.length; n--;) ssc_cache[ssc_uniqueID(e[n])] = t;
-  return t
-}
-
-function ssc_overflowingAncestor(e) {
-  var t = [];
-  var n = ssc_root.scrollHeight;
-  do {
-    var r = ssc_cache[ssc_uniqueID(e)];
-    if (r) {
-      return ssc_setCache(t, r)
-    }
-    t.push(e);
-    if (n === e.scrollHeight) {
-      if (!ssc_frame || ssc_root.clientHeight + 10 < n) {
-        return ssc_setCache(t, document.body)
-      }
-    } else if (e.clientHeight + 10 < e.scrollHeight) {
-      overflow = getComputedStyle(e, "").getPropertyValue("overflow");
-      if (overflow === "scroll" || overflow === "auto") {
-        return ssc_setCache(t, e)
-      }
-    }
-  } while (e = e.parentNode)
-}
-
-function ssc_addEvent(e, t, n) {
-  window.addEventListener(e, t, n || false)
-}
-
-function ssc_removeEvent(e, t, n) {
-  window.removeEventListener(e, t, n || false)
-}
-
-function ssc_isNodeName(e, t) {
-  return e.nodeName.toLowerCase() === t.toLowerCase()
-}
-
-function ssc_directionCheck(e, t) {
-  e = e > 0 ? 1 : -1;
-  t = t > 0 ? 1 : -1;
-  if (ssc_direction.x !== e || ssc_direction.y !== t) {
-    ssc_direction.x = e;
-    ssc_direction.y = t;
-    ssc_que = []
-  }
-}
-
-function ssc_pulse_(e) {
-  var t, n, r;
-  e = e * ssc_pulseScale;
-  if (e < 1) {
-    t = e - (1 - Math.exp(-e))
-  } else {
-    n = Math.exp(-1);
-    e -= 1;
-    r = 1 - Math.exp(-e);
-    t = n + r * (1 - n)
-  }
-  return t * ssc_pulseNormalize
-}
-
-function ssc_pulse(e) {
-  if (e >= 1) return 1;
-  if (e <= 0) return 0;
-  if (ssc_pulseNormalize == 1) {
-    ssc_pulseNormalize /= ssc_pulse_(1)
-  }
-  return ssc_pulse_(e)
-}
-
-var ssc_framerate = 150;
-var ssc_animtime = 500;
-var ssc_stepsize = 150;
-var ssc_pulseAlgorithm = true;
-var ssc_pulseScale = 6;
-var ssc_pulseNormalize = 1;
-var ssc_keyboardsupport = true;
-var ssc_arrowscroll = 50;
-var ssc_frame = false;
-var ssc_direction = {
-  x: 0,
-  y: 0
-};
-
-var ssc_initdone = false;
-var ssc_fixedback = true;
-var ssc_root = document.documentElement;
-var ssc_activeElement;
-var ssc_key = {
-  left: 37,
-  up: 38,
-  right: 39,
-  down: 40,
-  spacebar: 32,
-  pageup: 33,
-  pagedown: 34,
-  end: 35,
-  home: 36
-};
-
-var ssc_que = [];
-var ssc_pending = false;
-var ssc_cache = {};
-
-setInterval(function() {
-  ssc_cache = {}
-}, 10 * 1e3);
-
-var ssc_uniqueID = function() {
-  var e = 0;
-  return function(t) {
-    return t.ssc_uniqueID || (t.ssc_uniqueID = e++)
-  }
-}();
-
-var ischrome = /chrome/.test(navigator.userAgent.toLowerCase());
-
-if (ischrome) {
-  ssc_addEvent("mousedown", ssc_mousedown);
-  ssc_addEvent("mousewheel", ssc_wheel);
-  ssc_addEvent("load", ssc_init)
-}
+// Initialize all .smoothScroll links
+jQuery(function($){ $.localScroll({filter:'.smoothScroll'}); });
